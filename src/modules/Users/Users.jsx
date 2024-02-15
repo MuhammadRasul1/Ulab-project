6595de4a7076bb2d8689ce83ee3484bd7539749d
Мухаммад Расул Кобилов <muhammadrasul020905@gmail.com>
2024-02-15 18:17:40 +0500

    add logout btn

--- src/modules/Users/Users.jsx
+++ src/modules/Users/Users.jsx
@@ -1,3 +1,15 @@
+import { Box, Button } from "@chakra-ui/react"
+import { authStore } from "store/auth.store"
+
 export const Users = () => {
-    return <h1>Users</h1>
+    const handleLogOut = () => {
+        authStore.userData = {}
+        authStore.logout()
+      }
+    return (
+        <Box>
+            <h1 style={{fontSize: "30px"}}>Users</h1>
+            <Button background={"red"} color={"white"} onClick={handleLogOut}>LOG OUT</Button>
+        </Box>
+    )
 }
