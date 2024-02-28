7ac93135f753eee3f44a7df9dcc9e4d8695371b4
Мухаммад Расул Кобилов <muhammadrasul020905@gmail.com>
2024-02-28 15:28:23 +0500

    test 123

--- src/modules/Admin/routes/index.jsx
+++ src/modules/Admin/routes/index.jsx
@@ -6,11 +6,13 @@ import { CoursesDetail } from "./Courses/CoursesDetail";
 import { ProfileDetail } from "./ProfileDetail";
 
 export const AdminRoutes = () => {
-  return <Routes>
-    <Route path="users" element={<Users />} />
-    <Route path="profile" element={<Profile />} />
-    <Route path="profile/detail" element={<ProfileDetail />} />
-    <Route path="courses" element={<CoursesMain />} />
-    <Route path="courses/:id" element={<CoursesDetail />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="users" element={<Users />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="profile/detail" element={<ProfileDetail />} />
+      <Route path="courses" element={<CoursesMain />} />
+      <Route path="courses/:id" element={<CoursesDetail />} />
+    </Routes>
+  );
 };
